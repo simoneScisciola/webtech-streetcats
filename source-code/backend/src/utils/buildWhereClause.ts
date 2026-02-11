5bfcbc9de7bfbddc29fa7f8b46001d79ee93685b
Simone Scisciola <simone.scisciola03@gmail.com>
2026-02-12 00:23:13 +0100

    Refactor buildWhereClause to support multi-value filters
    + Add allowInOp to FilterMapping
    + Update buildWhereClause to delegate filter handling to addWhereFilters. It now supports comma-separated multi-value filters using Op.in when allowInOp is enabled.

--- source-code/backend/src/utils/buildWhereClause.ts
+++ source-code/backend/src/utils/buildWhereClause.ts
@@ -17,13 +17,14 @@ export interface FilterMapping {
     type: 'string' | 'number' | 'boolean' | 'date';
     column: string; // Model column name
     op?: symbol; // Sequelize operator (Op.gte, Op.lte, ...)
+    allowInOp?: boolean; // Whether to allow the use of Op.in operator for multiple values (e.g. citizenship=Italy,USA,Canada)
 }
 
 /**
  * Builds the `where` clause for a Sequelize query
- * @param receivedFilters Object representing a map of filters received as query parameters (e.g. { name: "John", age: "30" })
+ * @param receivedFilters Object representing a map of filters received as query parameters (e.g. { name: "John", age: "30", citizenship="Italy,USA,Canada" })
  * @param desiredFilters Object representing a map of filters that we actually want to expose (e.g. { name: { type: "string", column: "name" }, ageGreaterThen: { type: "number", column: "age", op: Op.gte } })
- * @returns The Sequelize-friendly `where` query option
+ * @returns The ready-to-use Sequelize `where` query option
  */
 export function buildWhereClause(receivedFilters: ParsedFilters, desiredFilters: Record<string, FilterMapping>): WhereOptions {
 
@@ -35,70 +36,68 @@ export function buildWhereClause(receivedFilters: ParsedFilters, desiredFilters:
 
         // If receivedFilers[filter] is not empty and desiredMapping[filter] exists
         if (receivedFilters[filter] !== "" && desiredFilters[filter]) {
-
-            // Retrieve received fiter value
-            const filterValue = receivedFilters[filter];
-            
-            // Attempt to add where clause (based on expected type)
-            switch (desiredFilters[filter].type) {
-                case 'string':
-                    buildStringWhereClause(where, desiredFilters[filter], filterValue);
-                    break;
-                case 'number':
-                    buildNumberWhereClause(where, desiredFilters[filter], filterValue);
-                    break;
-                case 'boolean':
-                    buildBooleanWhereClause(where, desiredFilters[filter], filterValue);
-                    break;
-                case 'date':
-                    buildDateWhereClause(where, desiredFilters[filter], filterValue);
-                    break;
-            }
+            addWhereFilters(where, desiredFilters[filter], receivedFilters[filter]);
         }
     }
 
     return where;
 }
 
-function buildStringWhereClause(where: Record<string, any>, desiredFilter: FilterMapping, filterValue: string) {
-    let column = desiredFilter.column;
-    let op = desiredFilter.op;
+function addWhereFilters(where: Record<string, any>, desiredFilter: FilterMapping, filterValue: string) {
 
-    where[column] = { [op ?? Op.eq]: filterValue }; // Uses custom op (or Op.eq by default) for exact match on val
+    // Retrieve desired filter settings
+    const { column, type, op, allowInOp } = desiredFilter;
+
+    // If filter value is a list of values separated by ","
+    const filterValuesList = allowInOp && filterValue.includes(',')
+        ? filterValue.split(',').map(filterValue => filterValue.trim()) // Split by separator and delete starting and trailing whitespaces
+        : [filterValue]; // Filter value is a single value
+
+    // Attempt to parse each filter value, based on expected type
+    const parsedFilterValues = filterValuesList.map(filterValue => parseFilterValueByType(type, filterValue, column));
+
+    
+    // Every filter is combined through the "AND" logical operator
+
+    // Use Op.in if filter value is a list of values (e.g. citizenship=Italy,USA,Canada)
+    if (parsedFilterValues.length > 1) {
+        where[column] = {
+            [Op.in]: parsedFilterValues
+        };
+    } else { // Use custom Op (or Op.eq by default) for exact match on value (e.g. name=John)
+        where[column] = {
+            [op ?? Op.eq]: parsedFilterValues[0] 
+        };
+    }
 }
 
-function buildNumberWhereClause(where: Record<string, any>, desiredFilter: FilterMapping, filterValue: string) {
-    let column = desiredFilter.column;
-    let op = desiredFilter.op;
+function parseFilterValueByType(type: FilterMapping['type'], filterValue: string, column: string): any {
+    switch (type) {
+        case 'string':
+            return filterValue;
 
-    let number = Number(filterValue); // Converts to number
+        case 'number': {
+            const numberValue = Number(filterValue); // Converts to number
 
-    if (!Number.isNaN(number))
-        where[column] = { [op ?? Op.eq]: number }; // Uses custom op (or Op.eq by default) for exact match on val
-    else
-        throw new createError.BadRequest(`"${column}" query parameter must be a number.`);
-}
+            if (Number.isNaN(numberValue))
+                throw new createError.BadRequest(`"${column}" query parameter must be a number.`);
+            return numberValue;
+        }
 
-function buildBooleanWhereClause(where: Record<string, any>, desiredFilter: FilterMapping, filterValue: string) {
-    let column = desiredFilter.column;
-    let op = desiredFilter.op;
+        case 'boolean': {
+            const booleanValue = filterValue.toLowerCase();
 
-    let bool = filterValue.toLowerCase();
+            if (booleanValue !== 'true' && booleanValue !== 'false')
+                throw new createError.BadRequest(`"${column}" query parameter must be boolean.`);
+            return booleanValue === 'true';
+        }
 
-    if (bool === 'true' || bool === 'false')
-        where[column] = { [op ?? Op.eq]: (bool === 'true') }; // Uses custom op (or Op.eq by default) for exact match on val
-    else
-        throw new createError.BadRequest(`"${column}" query parameter must be boolean.`);
-}
+        case 'date': {
+            const dateValue = new Date(filterValue);
 
-function buildDateWhereClause(where: Record<string, any>, desiredFilter: FilterMapping, filterValue: string) {
-    let column = desiredFilter.column;
-    let op = desiredFilter.op;
-
-    let date = new Date(filterValue);
-
-    if (!Number.isNaN(date.getTime()))
-        where[column] = { [op ?? Op.eq]: date }; // Uses custom op (or Op.eq by default) for exact match on val
-    else
-        throw new createError.BadRequest(`"${column}" query parameter must be a date.`);
+            if (Number.isNaN(dateValue.getTime()))
+                throw new createError.BadRequest(`"${column}" query parameter must be a date.`);
+            return dateValue;
+        }
+    }
 }
