a4027e24d6f842ee92c2ae5715c1293593eb2b19
Simone Scisciola <simone.scisciola03@gmail.com>
2026-02-16 21:48:39 +0100

    Changed colors
    + Changed color palette
    + Moved config.service into dedicated folder

--- source-code/frontend/src/app/app.config.ts
+++ source-code/frontend/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { ConfigService } from './core/config.service';
+import { ConfigService } from './core/services/config.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
