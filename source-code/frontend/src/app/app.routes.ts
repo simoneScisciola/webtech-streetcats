11306031064bf4e056e1a93ca8c63c4a2c13b09f
Simone Scisciola <simone.scisciola03@gmail.com>
2026-02-25 15:35:26 +0100

    Deleted example page
    + Deleted example page component
    + Deleted example route

--- source-code/frontend/src/app/app.routes.ts
+++ source-code/frontend/src/app/app.routes.ts
@@ -2,38 +2,32 @@ import { Routes } from '@angular/router';
 
 import { Home } from '#features/home/home';
 import { SightingsMap } from '#features/sightings-map/sightings-map';
-import { Example } from '#features/example/example';
 import { Login } from '#features/auth/login/login';
 import { Signup } from '#features/auth/signup/signup';
 
 export const routes: Routes = [
-    {
-       path: 'home',
-       title: "Webtech's Streetcats",
-       component: Home
-    },
-    {
-       path: 'sightings-map',
-       title: "Sightings Map",
-       component: SightingsMap
-    },
-    {
-       path: 'log-in',
-       title: "Log In",
-       component: Login
-    },
-    {
-       path: 'sign-up',
-       title: "Sign Up",
-       component: Signup
-    },
-    {
-       path: 'example',
-       title: "Example",
-       component: Example
-    },
-    {
-      path: '**',
-      redirectTo: '/home'
-    }
+  {
+    path: 'home',
+    title: "Webtech's Streetcats",
+    component: Home
+  },
+  {
+    path: 'sightings-map',
+    title: "Sightings Map",
+    component: SightingsMap
+  },
+  {
+    path: 'log-in',
+    title: "Log In",
+    component: Login
+  },
+  {
+    path: 'sign-up',
+    title: "Sign Up",
+    component: Signup
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
 ];
