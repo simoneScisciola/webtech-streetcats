3ff7b4fbca9619dbbdf325cf19b7d60e58edec34
Simone Scisciola <simone.scisciola03@gmail.com>
2026-03-16 23:50:07 +0100

    Updated home

--- source-code/frontend/src/app/features/home/home.ts
+++ source-code/frontend/src/app/features/home/home.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faMapLocationDot, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, FaIconComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+
+  // Icons
+  readonly icons = {
+    map: faMapLocationDot,
+    join: faUserPlus
+  };
 
 }
